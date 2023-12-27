c26d019dc2bcb12283a191a603a076063891a2f7
tuann <kietnguyen8102@gmail.com>
2023-12-27 21:54:54 +0700

    [BE] API CRUD thành viên

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -3,11 +3,9 @@ const userController = require('../controllers/user.controller');
 const groupController = require('../controllers/group.controller');
 const memberController = require('../controllers/member.controller');
 const router = express.Router();
-
 // user
 router.get('/user', userController.getAll);
 router.post('/login', userController.login);
-
 // group
 router.get('/group/:idUser', groupController.getAll);
 router.post('/group/add', groupController.add);
@@ -15,4 +13,5 @@ router.post('/group/add', groupController.add);
 // member
 router.post('/member/add', memberController.add)
 router.post('/member/delete', memberController.delete)
+
 module.exports = router
