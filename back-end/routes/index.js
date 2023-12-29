66034a8fbaf0281c8fe4097b28ea6dd457b5c17d
tuann <kietnguyen8102@gmail.com>
2023-12-29 21:13:38 +0700

    [BE] API quản lý kho

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const userController = require('../controllers/user.controller');
 const groupController = require('../controllers/group.controller');
 const memberController = require('../controllers/member.controller');
+const foodController = require('../controllers/food.controller');
 const marketController = require('../controllers/market.controller');
 const recipeController = require('../controllers/recipe.controller');
-const foodController = require('../controllers/food.controller')
+const storeController = require('../controllers/store.controller');
 
 const router = express.Router();
 
@@ -22,19 +23,23 @@ router.post('/group/market-share/add', groupController.addShare);
 router.post('/member/add', memberController.add)
 router.post('/member/delete', memberController.delete)
 
-// market
-router.get('/market/:idUser', marketController.getAll);
-router.post('/market/add', marketController.add);
-router.post('/market/buy', marketController.buy);
-
 // food
 router.post('/food', foodController.getAll);
 router.post('/food/add', foodController.add);
 router.get('/food/delete/:idFood', foodController.delete);
 
+// market
+router.get('/market/:idUser', marketController.getAll);
+router.post('/market/add', marketController.add);
+router.post('/market/buy', marketController.buy);
+
 // recipe
 router.post('/recipe', recipeController.getAll)
 router.post('/recipe/add', recipeController.add)
+
+// store
+router.get('/store/:idUser', storeController.getAll)
+router.get('/store/delete/:id', storeController.delete)
 
 
 module.exports = router
